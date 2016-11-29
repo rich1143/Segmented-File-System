93a334334f25e9bd3b957bf0dbe6dbdf302c85ea
rich1143 <rich1143@morris.umn.edu>
2016-11-29 11:57:18 -0600

    Added class files (Packet, HeaderPacket, DataPacket, and File) and started working on Main

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,16 @@
 package segmentedfilesystem;
+import java.io.*;
 
 public class Main {
-    
+
     public static void main(String[] args) {
-        
+
+      DatagramSocket socket = new DatagramSocket();
+      byte[] buf = new byte[256];
+      InetAddress address = InetAddress.getByName("sputnik");
+      DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 6014);
+      socket.send(packet);
+
     }
 
 }
