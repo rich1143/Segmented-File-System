59ac9b2d6d782cb715e8839e8f38097f1adf5683
rich1143 <rich1143@morris.umn.edu>
2016-12-01 10:20:52 -0600

    Made changes to Main.java

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,13 +1,19 @@
 package segmentedfilesystem;
 import java.io.*;
+import java.net.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
+    if (args.length != 1) {
+      System.out.println("Usage: java QuoteClient <hostname>");
+      return;
+    }
+
       DatagramSocket socket = new DatagramSocket();
       byte[] buf = new byte[256];
-      InetAddress address = InetAddress.getByName("sputnik");
+      InetAddress address = InetAddress.getByName(args[0]);
       DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 6014);
       socket.send(packet);
 
