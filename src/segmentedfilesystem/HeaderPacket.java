94b34e0d881280b377b0005734c79d1315a23860
Ai Sano <sanox013@bebop.morris.umn.edu>
2016-12-01 11:50:26 -0600

    Added packages to class files Worked on receiving packets from the server

--- src/segmentedfilesystem/HeaderPacket.java
+++ src/segmentedfilesystem/HeaderPacket.java
@@ -1,3 +1,5 @@
+package segmentedfilesystem;
+
 public class HeaderPacket {
 
 }
--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -4,19 +4,31 @@ import java.net.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
     if (args.length != 1) {
-      System.out.println("Usage: java QuoteClient <hostname>");
+      System.out.println("You need to enter a hostname.");
       return;
     }
 
       DatagramSocket socket = new DatagramSocket();
       byte[] buf = new byte[256];
       InetAddress address = InetAddress.getByName(args[0]);
-      DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 6014);
-      socket.send(packet);
-
+      DatagramPacket sendPacket = new DatagramPacket(buf, buf.length, address, 6014);
+      socket.send(sendPacket);
+      
+      boolean keepReceiving = true;
+      while (keepReceiving) {
+    	  DatagramPacket receivePacket = new DatagramPacket(buf, buf.length);
+    	  socket.receive(receivePacket);
+    	  byte[] received = receivePacket.getData();
+    	  
+    	  if (received == null || received.length == 0) {
+    		  keepReceiving = false;
+    	  }
+      }
+    	  
+      socket.close();
     }
 
 }
